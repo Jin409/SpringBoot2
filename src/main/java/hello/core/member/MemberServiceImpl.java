ba1fb6629c4323b2494435484c3d1502e90fb158
Jin409 <sualng123@gmail.com>
2022-07-04 22:02:59 +0900

    section 2

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,18 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    public static void main(String[] args) {
+//
+//    }
+
+    // 추상화에만 의존하는 것
+    private final MemberRepository memberRepository;
+
+    // Repository의 구현체가 뭐가 들어갈지를 생성자를 통해서 주입시켜줌.
+    // setter
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,12 +3,27 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private DiscountPolicy discountPolicy;
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private DiscountPolicy discountpolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public void OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    //로미오와 줄리엣에서 로미오 역을 맡은 배우가 줄리엣 역을 맡을 배우를 고르는 것과 같음
+    // DIP 위반! => 항상 추상화에 의존해라
+    // discountpolicy는 인터페이스 => 직접 객체인 fixdiscountpolicy 생성
+    // 의도한 바는, 지금의 OrderServiceImpl이 DiscountPolicy 라는 인터페이스에 의존하는 것.
+    // 그러나, 현재 이미 구체화 되어 있는 RateDiscountPolicy에도 의존하고 있음.
+    // 이렇게 의존 관계가 있으면, RateDiscountPolicy를 FixDiscountPolicy로 바꾸는 순간 아래의 소스 코드 전부를 바꿔야 함.
+    // => 객체지향적이지 못함!
+    // private DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
